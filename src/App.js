1d98be3aac54a6285dfd43a0741f6d2944c26f1f
wegs <manansalaludwig@gmail.com>
2020-08-13 17:09:39 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,148 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Modal from "./components/Modal";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      viewCompleted: false,
+      activeItem: {
+        patient: "",
+        comments: "",
+        start_date: "",
+        end_date:"",
+        completed: false
+      },
+      appList: []
+    };
+  }
+  componentDidMount() {
+    this.refreshList();
+  }
+  refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/appointments/")
+      .then(res => this.setState({ appList: res.data }))
+      .catch(err => console.log(err));
+  };
+  displayCompleted = status => {
+    if (status) {
+      return this.setState({ viewCompleted: true });
+    }
+    return this.setState({ viewCompleted: false });
+  };
+  renderTabList = () => {
+    return (
+      <div className="my-5 tab-list">
+        <span
+          onClick={() => this.displayCompleted(true)}
+          className={this.state.viewCompleted ? "active" : ""}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          complete
+        </span>
+        <span
+          onClick={() => this.displayCompleted(false)}
+          className={this.state.viewCompleted ? "" : "active"}
+        >
+          Incomplete
+        </span>
+      </div>
+    );
+  };
+  renderItems = () => {
+    const { viewCompleted } = this.state;
+    const newItems = this.state.appList.filter(
+      item => item.completed === viewCompleted
+    );
+    return newItems.map(item => (
+      <li
+        key={item.id}
+        className="list-group-item d-flex justify-content-between align-items-center"
+      >
+        <span
+          className={`todo-title mr-2 ${
+            this.state.viewCompleted ? "completed-todo" : ""
+          }`}
+          title={item.comment}
+        >
+          {item.patient}
+        </span>
+        <span>
+          <button
+            onClick={() => this.editItem(item)}
+            className="btn btn-secondary mr-2"
+          >
+            {" "}
+            Edit{" "}
+          </button>
+          <button
+            onClick={() => this.handleDelete(item)}
+            className="btn btn-danger"
+          >
+            Delete{" "}
+          </button>
+        </span>
+      </li>
+    ));
+  };
+  toggle = () => {
+    this.setState({ modal: !this.state.modal });
+  };
+  handleSubmit = item => {
+    console.log(item)
+    this.toggle();
+    if (item.id) {
+      axios
+        .put(`http://localhost:8000/api/appointments/${item.id}/`, item)
+        .then(res => this.refreshList());
+      return;
+    }
+    axios
+      .post("http://localhost:8000/api/appointments/", item)
+      .then(res => this.refreshList())
+      .catch(err => console.log(err));
+  };
+  handleDelete = item => {
+    axios
+      .delete(`http://localhost:8000/api/appointments/${item.id}`)
+      .then(res => this.refreshList());
+  };
+  createItem = () => {
+    const item = { patient: "", start_date: "", end_date: "", comments: "", completed: false };
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  editItem = item => {
+    this.setState({ activeItem: item, modal: !this.state.modal });
+  };
+  render() {
+    return (
+      <main className="content">
+        <h1 className="text-white text-uppercase text-center my-4">Appointments</h1>
+        <div className="row ">
+          <div className="col-md-6 col-sm-10 mx-auto p-0">
+            <div className="card p-3">
+              <div className="">
+                <button onClick={this.createItem} className="btn btn-primary">
+                  Add Appointment
+                </button>
+              </div>
+              {this.renderTabList()}
+              <ul className="list-group list-group-flush">
+                {this.renderItems()}
+              </ul>
+            </div>
+          </div>
+        </div>
+        {this.state.modal ? (
+          <Modal
+            activeItem={this.state.activeItem}
+            toggle={this.toggle}
+            onSave={(item) => this.handleSubmit(item)}
+          />
+        ) : null}
+      </main>
+    );
+  }
 }
-
 export default App;
---
+++ src/components/Modal.js
@@ -0,0 +1,97 @@
+ // frontend/src/components/Modal.js
+
+ import React, { Component } from "react";
+ import {
+   Button,
+   Modal,
+   ModalHeader,
+   ModalBody,
+   ModalFooter,
+   Form,
+   FormGroup,
+   Input,
+   Label
+ } from "reactstrap";
+
+ export default class CustomModal extends Component {
+   constructor(props) {
+     super(props);
+     this.state = {
+       activeItem: this.props.activeItem
+     };
+   }
+   handleChange = e => {
+     let { name, value } = e.target;
+     if (e.target.type === "checkbox") {
+       value = e.target.checked;
+     }
+     const activeItem = { ...this.state.activeItem, [name]: value };
+     this.setState({ activeItem });
+   };
+   render() {
+     const { toggle, onSave } = this.props;
+     return (
+       <Modal isOpen={true} toggle={toggle}>
+         <ModalHeader toggle={toggle}> Todo Item </ModalHeader>
+         <ModalBody>
+           <Form>
+             <FormGroup>
+               <Label for="patient">Patient</Label>
+               <Input
+                 type="text"
+                 name="patient"
+                 value={this.state.activeItem.patient}
+                 onChange={this.handleChange}
+                 placeholder="Enter Patient"
+               />
+             </FormGroup>
+             <FormGroup>
+               <Label for="start_date">Start Date</Label>
+               <Input
+                 type="datetime-local"
+                 name="start_date"
+                 value={this.state.activeItem.start_date}
+                 onChange={this.handleChange}
+               />
+             </FormGroup>
+             <FormGroup>
+               <Label for="end_date">End Date</Label>
+               <Input
+                 type="datetime-local"
+                 name="end_date"
+                 value={this.state.activeItem.end_date}
+                 onChange={this.handleChange}
+               />
+             </FormGroup>
+             <FormGroup>
+               <Label for="comments">Comments</Label>
+               <Input
+                 type="text"
+                 name="comments"
+                 value={this.state.activeItem.comments}
+                 onChange={this.handleChange}
+                 placeholder="Enter Comments"
+               />
+             </FormGroup>
+             <FormGroup check>
+               <Label for="completed">
+                 <Input
+                   type="checkbox"
+                   name="completed"
+                   checked={this.state.activeItem.completed}
+                   onChange={this.handleChange}
+                 />
+                 Completed
+               </Label>
+             </FormGroup>
+           </Form>
+         </ModalBody>
+         <ModalFooter>
+           <Button color="success" onClick={() => onSave(this.state.activeItem)}>
+             Save
+           </Button>
+         </ModalFooter>
+       </Modal>
+     );
+   }
+ }
